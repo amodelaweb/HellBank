4b84950f6bb5c02446b75a7c9c3d0036a7ea2f80
Santiago Chaustre <santiago.chaustreperdomo@gmail.com>
2018-08-30 13:31:58 -0500

    initial data base

---
+++ Database.php
@@ -0,0 +1,18 @@
+<?php
+include_once dirname(__FILE__) . '/config.php';
+
+class Database
+{
+    private $conn ;
+    public function connection()
+    {
+      $this->conn = null ;
+        try {
+            $this->conn = new PDO('mysql:host=' . HOST_DB . ';dbname=' . NOMBRE_DB, USUARIO_DB, USUARIO_PASS);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Error de coneccion : " .  $e->getMessage();
+        }
+         return $this->conn;
+    }
+}
---
+++ config/mail.php
@@ -0,0 +1,6 @@
+<?php
+  define('EMAIL' , 'mail@gmail.com');
+  define('E_PASSWORD' , 'SOYUNAPASS');
+  define('E_PORT', 465);
+
+ ?>
